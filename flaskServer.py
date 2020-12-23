912d6daae9c6f66c507f7b958c4315983a3e3be2
Sean Klein <63231107+seapat@users.noreply.github.com>
2020-12-23 16:17:35 +0100

    add placeholder sites

--- flaskServer.py
+++ flaskServer.py
@@ -1,4 +1,5 @@
 # Sean Klein 5575709
+
 from flask import Flask, render_template, redirect, url_for, request
 import pandas as pd
 
@@ -12,6 +13,14 @@ def index():
 @app.route('/base' , methods=["POST", "GET"])
 def base():
     return render_template('base.html')
+
+@app.route('/home' , methods=["POST", "GET"])
+def home():
+    return render_template('home.html')
+
+@app.route('/about' , methods=["POST", "GET"])
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
