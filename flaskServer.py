99fc93e2f35e895900924561f57808e68ca4ff95
Sean Klein <63231107+seapat@users.noreply.github.com>
2020-12-30 15:53:30 +0100

    copy paste pandas code (bad style?)

--- flaskServer.py
+++ flaskServer.py
@@ -38,7 +38,34 @@ def about():
 # Functions  #
 ##############
 
+def combine_tsv(data_meta = "ecosystem_Metadata.tsv", data_bacteria = "ecosystem_HITChip.tsv"):
+    # Additional Python Code to perform small computations
 
+    # merge two tsv files into one json file
+    # Input: 2 tsv files (1 metadata, 1 bacteria)
+    # output: 1 json file
+
+    import pandas as pd
+    metadata = pd.read_csv(data_meta, delimiter="\t")
+    bacteria = pd.read_csv(data_bacteria, delimiter="\t")
+    bacteria = bacteria.drop(bacteria.columns[[0]], axis=1)  # remove column with SampleIDs -> use Sample IDs from metadata.
+
+    # dataframe including all columns of both files in one level
+    all_data = pd.concat([metadata, bacteria], axis=1)
+    all_data.to_csv("All_Data.csv")
+    #print(all_data)
+
+    # Creating a new column "bacterias" within the metadata dataframe
+    # for each (i,bacterias) cell (i=0 -> len(metadata.SampleID)) insert the i-th row of bacteria
+    metadata['Bacteria'] = ""
+    metadata['Bacteria'] = metadata['Bacteria'].astype(object)
+    for index, row in metadata.iterrows():
+        metadata.at[index, 'Bacteria'] = bacteria.iloc[[index]]
+
+    metadata.to_csv("Complete_Data.csv")   # convert to csv
+    metadata = metadata.to_json("Complete_Data.json", orient="records")  # convert to json format, this takes a while.
+
+    return metadata
 
 if __name__ == '__main__':
     app.run(debug=True, port=6001)
