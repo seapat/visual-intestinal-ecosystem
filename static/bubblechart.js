6f3cf457a9c9d4e76b60a09b62f0dc028c4ed636
Sean Klein <63231107+seapat@users.noreply.github.com>
2020-12-30 20:47:11 +0100

    add y and x axis

--- static/bubblechart.js
+++ static/bubblechart.js
@@ -1,14 +1,20 @@
+//Sean Klein 5575709
+
+// gets the nested javascript object containing metadata and measurment data as an additional
+
+
 console.log(dataset);
 //console.log(Math.max.apply(Math, dataset.map(function(o) { return o.Age; })));
-//console.table(dataset); This take long to load upfront
+//console.table(dataset); //This takes a bit to load
 
 // DIMENSIONS //
-let margin = {top: 40, right: 150, bottom: 60, left: 30},
+let margin = {top: 100, right: 150, bottom: 60, left: 50},
     width = 1400 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
 
 // create intial svg
 let svg = d3.select("#bubblechart")
+    .data(dataset)
     .append('svg')
     .attr("width", width + margin.left+ margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -19,18 +25,48 @@ let svg = d3.select("#bubblechart")
 // Y AXIS //
 
     // y scale
-    let y = d3.scaleLinear()
-        .domain([0, Math.max.apply(Math, dataset.map(function(o) { return o.Age; }))]) //8 is next int after max again
+    let yScale = d3.scaleLinear()
+        //return max value from col "Age" https://stackoverflow.com/a/4020842/14276346
+        .domain([0, Math.max.apply(Math, dataset.map(function(o) { return o.Age; }))])
         .range([ height, 0]);
 
-    //y ticks
-    svg.append("g").call(d3.axisLeft().scale(y));
+    // y ticks
+    svg.append("g")
+        .call(d3.axisLeft()
+        .scale(yScale));
 
-    //Y Axis label
+    // y Axis label
     svg.append("text")
-        .attr("text-anchor", "end")
+        .attr("text-anchor", "start")
         .attr("x", -20 )
         .attr("y", -20 )
-        .text("Children born per woman")
-        .style("font-size", 12)
-        .attr("text-anchor", "start");
+        .text("Age")
+        //.style("font-size", 12);
+
+
+// X AXIS //
+
+    //extract nationality column (map), make values unique via new Set
+    let nationalities = new Set(dataset.map(d => d["Nationality"])) ;
+    nationalities.delete(null) //remove null in place
+    console.log(nationalities);
+
+
+    // x scale'
+    let xScale = d3.scaleBand()
+        .domain(nationalities)
+        .range([0, width]);
+
+    // x ticks
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")") //put zero to the bottom
+    .call(d3.axisBottom()
+        .scale(xScale));
+
+    // x axis label
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height + 50 )
+        .text("Nationality")
+        //.style("font-size", 12);
