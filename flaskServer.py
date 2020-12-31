3f03b2b283f30dc16fac72250febba54e7146806
Florian Kellner <mr.florian.kellner@web.de>
2020-12-31 15:40:57 +0100

    Data like described in paper
    one sample per subject, no multilevel json structure since unnecessary. SampleID no longer exported, but we can change that if necessary

--- flaskServer.py
+++ flaskServer.py
@@ -53,27 +53,29 @@ def combine_tsv(data_meta = "static/ecosystem_Metadata.tsv", data_bacteria = "st
     # Input: 2 tsv files (1 metadata, 1 bacteria)
     # output: 1 json file
 
-    import pandas as pd
-    metadata = pd.read_csv(data_meta, delimiter="\t")
-    bacteria = pd.read_csv(data_bacteria, delimiter="\t")
-    bacteria = bacteria.drop(bacteria.columns[[0]], axis=1)  # remove column with SampleIDs -> use Sample IDs from metadata.
+    metadata = pd.read_csv(data_meta, delimiter="\t", index_col="SampleID")
+    bacteria = pd.read_csv(data_bacteria, delimiter="\t", index_col="SampleID")
 
     # dataframe including all columns of both files in one level
     all_data = pd.concat([metadata, bacteria], axis=1)
     #all_data.to_csv("All_Data.csv")
     #print(all_data)
 
-    # Creating a new column "bacterias" within the metadata dataframe
-    # for each (i,bacterias) cell (i=0 -> len(metadata.SampleID)) insert the i-th row of bacteria
-    metadata['Bacteria'] = ""
-    metadata['Bacteria'] = metadata['Bacteria'].astype(object)
-    for index, row in metadata.iterrows():
-        metadata.at[index, 'Bacteria'] = bacteria.iloc[[index]]
+    # find out if we have more than one data point per subject (we do):
+    # all_data.groupby("SubjectID").size().to_frame().groupby(by=0).size()
+    # the paper says they took the first sample if several were available
+    # (see Methods -> Sample collection),
+    # so we should probably do the same:
+    subject_times = all_data[["SubjectID", "Time"]].groupby("SubjectID").min()
+    all_relevant_data = pd.merge(
+        left  = subject_times,
+        right = all_data,
+        how   ='inner',
+        on    = ["SubjectID", "Time"]
+    )
 
-    #metadata.to_csv("Complete_Data.csv")   # convert to csv
-    metadata = metadata.to_json(orient="records")  # convert to json format, this takes a while. # removed from to_json(): "Complete_Data.json",
-
-    return metadata
+    all_relevant_data.to_csv("Relevant_Data.csv")   # convert to csv
+    return all_relevant_data.to_json(orient="records")
 
 if __name__ == '__main__':
     app.run(debug=True, port=6001)
