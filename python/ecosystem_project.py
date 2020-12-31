1d073914ae4b37a091e2a1012c902b35d38548c7
Florian Kellner <mr.florian.kellner@web.de>
2020-12-31 14:36:14 +0100

    match rows by SampleID
    previously, row ids were based on the position in the file, but it is safer to double-check that both files are sorted by SampleID and have no gaps

--- python/ecosystem_project.py
+++ python/ecosystem_project.py
@@ -5,9 +5,8 @@
 # output: 1 json file
 
 import pandas as pd
-metadata = pd.read_csv("ecosystem_Metadata.tsv", delimiter="\t")
-bacteria = pd.read_csv("ecosystem_HITChip.tsv", delimiter="\t")
-bacteria = bacteria.drop(bacteria.columns[[0]], axis=1)  # remove column with SampleIDs -> use Sample IDs from metadata.
+metadata = pd.read_csv(data_meta, delimiter="\t", index_col="SampleID")
+bacteria = pd.read_csv(data_bacteria, delimiter="\t", index_col="SampleID")
 
 # dataframe including all columns of both files in one level
 all_data = pd.concat([metadata, bacteria], axis=1)
@@ -19,7 +18,7 @@ all_data.to_csv("All_Data.csv")
 metadata['Bacteria'] = ""
 metadata['Bacteria'] = metadata['Bacteria'].astype(object)
 for index, row in metadata.iterrows():
-    metadata.at[index, 'Bacteria'] = bacteria.iloc[[index]]
+    metadata.at[index, 'Bacteria'] = bacteria.loc[[index]]
 
 metadata.to_csv("Complete_Data.csv")   # convert to csv
 metadata = metadata.to_json("Complete_Data.json", orient="records")  # convert to json format, this takes a while.
