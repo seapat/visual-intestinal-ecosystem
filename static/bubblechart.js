fe9e5dfe7b23dc05acde796e4b53409f71690055
Sean Klein <63231107+seapat@users.noreply.github.com>
2021-01-04 22:54:30 +0100

    implement basic bubbles

--- static/bubblechart.js
+++ static/bubblechart.js
@@ -6,14 +6,80 @@
 
 console.log(dataset);
 
+/*
+grouped = d3.group(dataset, key => (key["Nationality"] == null) ? "Unknown" : key["Nationality"]);
+console.log(grouped);
+
+sortedByCount = d3.sort(grouped, d => d[1].length);
+console.log(sortedByCount);
+
+sortedByAge = d3.sort(grouped, d => d3.mean(d[1], value => value.Age));
+console.log(sortedByAge);
+
+counts = d3.rollup(dataset, value => value.length, key => (key["Nationality"] == null) ? "Unknown" : key["Nationality"]);
+console.log(counts);
+
+max = d3.max(
+    d3.rollup(dataset, value => value.length, key => (key["Nationality"] == null) ? "Unknown" : key["Nationality"]),
+    d => d[1]);
+console.log(max)
+
+console.log("separator")
+*/
+
+// FUNCTIONS //
+
+//creates map of different values inside a value, each row with that column is inside an array of that keys' value
+function groupBy (columnName) {
+    return d3.groups(dataset, key => (key[columnName] == null) ? "Unknown" : key[columnName])
+}
+console.log(groupBy("Nationality"))
+
+
+//takes a grouped map and secondary column by which the first level should be sorted by
+// if no secondary column is specified, will sort by amount of objects per group
+function sortByMean (groupedData, columnName=null) {
+    if (columnName != null) { 
+        return d3.sort(groupedData, d => d3.mean(d[1], value => value[columnName]));
+    }
+    else {
+        return d3.sort(groupedData, d => d[1].length);
+    }
+}
+console.log( sortByMean(groupBy("Nationality"), "Age"));
+console.log( sortByMean(groupBy("Nationality")));
+
+// extract rows by name and put into map, rename empty (null) cells to "Unkown"
+// Wrapping arrow functions into normal functions is prbably bad style...
+function extractColumn(columnName) {
+    return dataset.map(row => (row[columnName] == null) ? "Unknown" : row[columnName]);
+};
+console.log( extractColumn("Nationality"));
+
+// count occurences of values of given map https://stackoverflow.com/a/46090384/14276346
+function countOccurences(columnName) {
+    return d3.rollup(dataset, value => value.length, key => (key[columnName] == null) ? "Unknown" : key[columnName]);
+};
+console.log(countOccurences("Nationality"));
+// console.log(sortByMean(countOccurences("Nationality"), "Age"));
+
+//returns max of a given map with numbers as values
+function getMax(columnName) {
+    //return Math.max.apply(Math, Array.from( countsMap.values()));
+    return d3.max(
+        d3.rollup(dataset, value => value.length, key => (key[columnName] == null) ? "Unknown" : key[columnName]),
+        d => d[1]);
+};
+console.log(getMax("Nationality"));
+
 // VARIABLES //
 
-//extract nationality column, used for x axis and scaling bubble size, rename empty cells to "Unkown"
-let nationalities = dataset.map(d => (d["Nationality"] == null) ? "Unknown" : d["Nationality"]);
-
-
+//extract nationality column, used for x axis and scaling bubble size,
+let nationalities = extractColumn("Nationality"); //used for multiple operations
+let nationCounts = countOccurences(nationalities);
 
 // DIMENSIONS //
+
 let margin = {top: 100, right: 150, bottom: 60, left: 50},
     width = 1400 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
@@ -49,18 +115,18 @@ let svg = d3.select("#bubblechart")
         .text("Age")
         //.style("font-size", 12);
 
-
 // X AXIS //
-
-    //make values unique via new Set
-    let nationLabels = new Set(nationalities);
-    console.log(nationLabels)
-
-
-    // x scale'
-    let xScale = d3.scaleBand()
-        .domain(nationLabels) //TODO: sort occurence in some way (either by average age, bubble size or alphabet)
-        .range([0, width]);
+    
+    // x scale
+    let xScale = d3.scalePoint()
+        .domain(
+            Array.from( //create Array of Nation Names
+                sortByMean(groupBy("Nationality"), "Age"), //group by nation, sort nations by average age
+                nation => nation[0] //extract nation name
+                ) 
+            )
+        .range([0, width])
+        .padding([0.5]);
 
     // x ticks
     svg.append("g")
@@ -78,13 +144,32 @@ let svg = d3.select("#bubblechart")
 
 // DRAW BUBBLES //
 
-    // count occurences of nationalities https://stackoverflow.com/a/46090384/14276346
-    let nationCounts = nationalities.reduce((nationCounts, val) => nationCounts.set(val, 1 + (nationCounts.get(val) || 0)), new Map());
-    console.log(nationCounts)
-    console.log(Math.max(nationCounts.values())) //TODO: get max value across all cells
-    console.log(Math.max.apply(nationCounts))
+    /* 
+    console.log(Array.from( //create Array of Nation Names
+        sortByMean(groupBy("Nationality"), "Age"), //group by nation, sort nations by average age
+        nation => nation[1].length)) 
+        */
 
     // scale bubble size
     let bubbleScale = d3.scaleSqrt()
         .domain([1, 10])
-        .range([2 , 26]);
+        .range([1 , 10]); //getMax(nationCounts)
+
+
+        console.log(sortByMean(groupBy("Nationality"), "Age"))
+        svg.append('g')
+        .selectAll("dot")
+        .data(sortByMean(groupBy("Nationality"), "Age"))
+        .enter()
+        .append("circle")
+        .attr("class",  function(d) { 
+            return "bubbles " + d[0];}) //returns names of nations
+        .attr("cx", function (d) { 
+            return xScale(d[0]);}) //position according to names of nations
+        .attr("cy", function (d) { 
+            return yScale(d3.mean(d[1], d => d.Age)); } ) //loop through object/subjects per nation, calculate mean of Age
+        .attr("r", function (d) { 
+            return bubbleScale(d[1].length );}) //scale by size of of each nations array == amount of subjects/samples
+        
+        .style("opacity", "0.7")
+        .style("fill", "none;")
