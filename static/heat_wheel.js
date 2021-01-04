7490a3b5fb66e28c230987f7894fab4f44772075
Florian Kellner <mr.florian.kellner@web.de>
2021-01-04 15:19:32 +0100

    add some comments

--- static/heat_wheel.js
+++ static/heat_wheel.js
@@ -8,25 +8,30 @@ const MARGIN_X = 0;
 const SVG_X = (INNER_RADIUS + MARGIN_X) * 2;
 const SVG_Y = (INNER_RADIUS + MARGIN_Y) * 2;
 
+// helper function for rotating text
 function rad2dgr(radians) {
   return radians * (180 / Math.PI);
 }
 
+// prepare canvas
 const svg = d3.select('#heat_wheel')
   .attr('width', SVG_Y)
   .attr('height', SVG_X)
-  .attr('text-anchor', 'end')
+  .attr('text-anchor', 'end') // right-align text fields
   .attr('font-family', 'sans-serif')
   .append('g')
-  .attr('transform', `translate(${SVG_Y / 2},${SVG_X / 2})`);
+  .attr('transform', `translate(${SVG_Y / 2},${SVG_X / 2})`); // start drawing from the middle
 
-const bacteria_positions = d3.pie().value(d => 1)(species);
+// pie chart needed for calculating angles
+const bacteria_angles = d3.pie().value(d => 1)(species);
 
+// arc needed to compute positions for the legend
 const outer_circle = d3.arc()
   .innerRadius(0)
   .outerRadius(INNER_RADIUS * 2);
 
-bacteria_positions.map(d => {
+// create graph
+bacteria_angles.map(d => {
   svg.append('text')
      .attr('fill', 'black')
      .attr('transform', `translate(${outer_circle.centroid(d).join(',')}) rotate(${rad2dgr(d.startAngle) - 90})`)
