534ef8452bb55421ee5586c5280f1548043e1d4a
Florian Kellner <mr.florian.kellner@web.de>
2021-01-08 11:11:23 +0100

    flat structure worked into current version of main

--- python/ecosystem_project.py
+++ python/ecosystem_project.py
@@ -8,16 +8,12 @@ import pandas as pd
 metadata = pd.read_csv(data_meta, delimiter="\t", index_col="SampleID")
 bacteria = pd.read_csv(data_bacteria, delimiter="\t", index_col="SampleID")
 
+# normalize bacteria dataframe
+bacteria = (bacteria - bacteria.min())/(bacteria.max() - bacteria.min())
+
 # dataframe including all columns of both files in one level
 all_data = pd.concat([metadata, bacteria], axis=1)
 all_data.to_csv("All_Data.csv")
-
-# Creating a new column "bacterias" within the metadata dataframe
-# for each (i,bacterias) cell (i=0 -> len(metadata.SampleID)) insert the i-th row of bacteria
-metadata['Bacteria'] = ""
-metadata['Bacteria'] = metadata['Bacteria'].astype(object)
-for index, row in metadata.iterrows():
-    metadata.at[index, 'Bacteria'] = bacteria.loc[[index]]
 
 # find out if we have more than one data point per subject (we do):
 # all_data.groupby("SubjectID").size().to_frame().groupby(by=0).size()
@@ -27,12 +23,12 @@ for index, row in metadata.iterrows():
 subject_times = metadata[["SubjectID", "Time"]].groupby("SubjectID").min()
 # pd.merge removes indexes if they are not used for the join, so this step
 # needs to take place when we do not need the SampleIDs anymore.
-metadata = pd.merge(
+relevant_data = pd.merge(
     left  = subject_times,
-    right = metadata,
+    right = all_data,
     how   ='inner',
     on    = ["SubjectID", "Time"]
 )
 
-metadata.to_csv("Complete_Data.csv")   # convert to csv
-metadata = metadata.to_json("Complete_Data.json", orient="records")  # convert to json format, this takes a while.
+relevant_data.to_csv("Relevant_Data.csv")   # convert to csv
+relevant_data = relevant_data.to_json("Relevant_Data.json", orient="records")  # convert to json format, this takes a while.
