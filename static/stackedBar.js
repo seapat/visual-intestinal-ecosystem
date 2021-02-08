f0f04f85575b382dbd0813d3feaf3b665df9b957
Sean Klein <63231107+seapat@users.noreply.github.com>
2021-02-08 13:31:54 +0100

    fixed ordering, hiding of legend if redundant axis

--- static/stackedBar.js
+++ static/stackedBar.js
@@ -11,8 +11,6 @@ let svg = d3.select("#stacked_bar")
     .append('svg')
     .attr('viewBox', `0 0 ${width + margin.left+ margin.right} ${height + margin.top + margin.bottom}`)
     .attr('style', `max-width: ${width + margin.left + margin.right}px;`)
-    // .attr("width", width + margin.left+ margin.right)
-    // .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
@@ -84,14 +82,20 @@ content.on('change', function(event) {
 // sorting of objects based on the passd attribute, uses array-inidces for custom sorting.
 function customSort(data, attribute) {
 
-    let KeyOrder = [
-        'underweight', 'lean', 'overweight', 'obese', 'severeobese', 'morbidobese',
-        'r', 'o', 'p',
-        'Unknown', null, NaN] // put bad values at the end
+    let KeyOrder = 
+    // sort by age, get dsitinct values from Age_group
+    dataset.sort((a, b) => (a.Age > b.Age) ? 1 : -1).map(item => item['Age_group']).filter( (value, index, self) => self.indexOf(value) === index)
+    // sort by Diversity, get Distinct values from Diversity_group
+    .concat(dataset.sort((a, b) => (a.Diversity > b.Diversity) ? 1 : -1).map(item => item['Diversity_group']).filter( (value, index, self) => self.indexOf(value) === index))
+    // implied sorting for catergorical values
+    .concat(['underweight', 'lean', 'overweight', 'obese', 'severeobese', 'morbidobese',
+            'r', 'o', 'p',
+            // put 'bad' values at the end)
+            'Unknown', null, NaN]) 
+  
 
     //check if column of first object, holds true for all objects in theory since null-values are handeled
     if (attribute in data[0]) {
-
         //uses the index
         return data.sort( (a, b) => KeyOrder.indexOf(a[attribute]) - KeyOrder.indexOf(b[attribute]));
     }
@@ -102,6 +106,10 @@ function customSort(data, attribute) {
 
 
 function drawGraph(xAttr, colorAttr) {
+
+    const identicalVars = (xAttr === colorAttr)
+    console.log(identicalVars)
+
 
     dataset = customSort(dataset, xAttr)
 
@@ -181,7 +189,6 @@ function drawGraph(xAttr, colorAttr) {
             .attr("x", -20 )
             .attr("y", -20 )
             .text("Subjects")
-            //.style("font-size", 12);
 
     // X AXIS //
 
@@ -203,7 +210,6 @@ function drawGraph(xAttr, colorAttr) {
             .attr("x", width)
             .attr("y", height + 50 )
             .text(xAttr)
-            //.style("font-size", 12);
 
     // DRAW BARS //
 
@@ -261,14 +267,6 @@ function drawGraph(xAttr, colorAttr) {
         .style("visibility","hidden");
 
     function whileMouseOver(event,d){
-        // console.log(d); // range of current stack
-        // console.log(d.data); //data of whole bar as object
-        // console.log(d3.pointer(event)); //coords of mous pointer
-
-        // console.log(d3.select(this))  // returns some ??? object
-        // console.log(d3.select(event.currentTarget)); // same as select.this()
-        // console.log(d3.select(event.currentTarget).node()); //returns svg of stack
-        // console.log(event.target.__data)
 
     tooltip
         .style("visibility","visible")
@@ -301,7 +299,6 @@ function drawGraph(xAttr, colorAttr) {
         .attr("x", width)
         .attr("width", 20)
         .attr("height", 20)
-        // .attr("stroke", "grey")
         .style("fill", color);
 
     legend.append("text")
@@ -310,6 +307,8 @@ function drawGraph(xAttr, colorAttr) {
         .attr("dy", ".35em")
         .style("text-anchor", "end")
         .text(function(d) { return d; });
+
+    legend.style( 'visibility', identicalVars ? "hidden" : 'visible')
 
     svg.selectAll("legend.title")
         .data([0]) //empty data to draw only once
@@ -320,4 +319,5 @@ function drawGraph(xAttr, colorAttr) {
         .attr("x", width + 20)
         .attr("y", 0)
         .style("text-anchor", "end")
+        .style( 'visibility', identicalVars ? "hidden" : 'visible')
 };
